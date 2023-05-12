6584d568a2a33a09c28fb063df1cfe144384181f
Buildkite Machine User <133214662+remnote-buildkite-machine-user@users.noreply.github.com>
2023-05-12 10:57:35 +0000

    Updated english files automatically

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,5 +1,10 @@
-import { BaseTranslation } from "../i18n-types";
+import { BaseTranslation } from '../i18n-types';
 
-const en: BaseTranslation = { }
+const en: BaseTranslation = {
+  // Sidebar
+  SIDEBAR_LABEL_ALL_NOTES: 'All Notes',
+  SIDEBAR_LABEL_TODAYS_NOTE: `Today's Note`,
+  SIDEBAR_LABEL_FLASHCARDS: 'Flashcards',
+};
 
 export default en;
