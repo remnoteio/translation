7d76900d4c1fd2c0170fedb39ce1b020d76a23de
Dennis W <7500200+dewil-official@users.noreply.github.com>
2023-05-17 12:32:18 +0200

    Cleanup & switched to json

--- inlang.config.js
+++ inlang.config.js
@@ -1,16 +1,14 @@
 export async function defineConfig(env) {
-  // initialize `typesafe-i18n` plugin
-  const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/ivanhofer/inlang-plugin-typesafe-i18n/dist/index.js"
-  )
-
-  // get the locale information from `typesafe-i18n`
-  const { base, locales } = await plugin.getLocaleInformation(env.$fs)
+  const { default: jsonPlugin } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js"
+  );
 
   return {
-    referenceLanguage: base,
-    languages: locales,
-    readResources: (args) => plugin.readResources({ ...args, ...env }),
-    writeResources: (args) => plugin.writeResources({ ...args, ...env }),
-  }
+    referenceLanguage: "en",
+    plugins: [
+      jsonPlugin({
+        pathPattern: "./{language}/*.json",
+      })
+    ]
+  };
 }
--- src/i18n/en/index.ts
+++
@@ -1,5 +0,0 @@
-import { BaseTranslation } from "../i18n-types";
-
-const en: BaseTranslation = { }
-
-export default en;
