5080a46c5d5224ad8dee60ee58585a34b582e644
Dennis W <7500200+dewil-official@users.noreply.github.com>
2023-07-14 09:32:47 +0200

    Try proper json config

--- inlang.config.js
+++ inlang.config.js
@@ -1,7 +1,10 @@
 export async function defineConfig(env) {
-  const { default: i18nextPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
-  );
+  // const { default: i18nextPlugin } = await env.$import(
+	// 	"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
+  // );
+	const { default: jsonPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js",
+	)
 
   const { default: standardLintRules } = await env.$import(
     "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
@@ -11,9 +14,12 @@ export async function defineConfig(env) {
     referenceLanguage: "en",
     
     plugins: [
-      i18nextPlugin({
-        pathPattern: "./{language}/*.json",
-      }),
+      // i18nextPlugin({
+      //   pathPattern: "./{language}/*.json",
+      // }),
+			jsonPlugin({
+				pathPattern: "./{language}/*.json",
+			}),
       standardLintRules()
     ]
   };
