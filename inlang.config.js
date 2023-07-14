fb3b68b9b40b29b014f87e480ae97ecf7008346e
Dennis W <7500200+dewil-official@users.noreply.github.com>
2023-07-14 09:43:57 +0200

    Added back version numbers

--- inlang.config.js
+++ inlang.config.js
@@ -1,10 +1,10 @@
 export async function defineConfig(env) {
   const { default: i18nextPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next/dist/index.js",
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
   );
 
   const { default: standardLintRules } = await env.$import(
-    "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules/dist/index.js",
+    "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
   );
 
   return {
