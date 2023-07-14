759188af45716157088f6221ffb08456ff1f0748
Dennis W <7500200+dewil-official@users.noreply.github.com>
2023-07-14 09:34:02 +0200

    Try without standardLintRules

--- inlang.config.js
+++ inlang.config.js
@@ -1,26 +1,26 @@
 export async function defineConfig(env) {
-  // const { default: i18nextPlugin } = await env.$import(
-	// 	"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
-  // );
-	const { default: jsonPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js",
-	)
-
-  const { default: standardLintRules } = await env.$import(
-    "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
+  const { default: i18nextPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
   );
+	// const { default: jsonPlugin } = await env.$import(
+	// 	"https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js",
+	// )
+
+  // const { default: standardLintRules } = await env.$import(
+  //   "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
+  // );
 
   return {
     referenceLanguage: "en",
     
     plugins: [
-      // i18nextPlugin({
-      //   pathPattern: "./{language}/*.json",
-      // }),
-			jsonPlugin({
-				pathPattern: "./{language}/*.json",
-			}),
-      standardLintRules()
+      i18nextPlugin({
+        pathPattern: "./{language}/*.json",
+      }),
+			// jsonPlugin({
+			// 	pathPattern: "./{language}/*.json",
+			// }),
+      // standardLintRules()
     ]
   };
 }
